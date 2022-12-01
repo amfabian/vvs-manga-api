7519cd8d10a032536dacf85355a9e71f9984ce00
Alexandre Fabian <xandy.fabian@gmail.com>
2022-12-01 15:57:58 -0300

    add sonar e testes

---
+++ src/test/java/ResourceTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class ResourceTest {
+    
+    @Test
+    @DisplayName("Teste 1")
+    void test(){
+
+        given()
+            .when().get()
+            .then()
+                .statusCode(200);
+
+    }
+    @Test
+    @DisplayName("Teste 1")
+    void testErrorPath(){
+
+        given()
+            .when().get("/manga")
+            .then()
+                .statusCode(404);
+
+    }
+}
