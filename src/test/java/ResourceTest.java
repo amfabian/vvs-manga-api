99e45204c6d7f5be1e7cebf4bea8752ae4fdd0e2
Alexandre Fabian <xandy.fabian@gmail.com>
2022-12-01 20:46:10 -0300

    update on ResourceTest

--- src/test/java/ResourceTest.java
+++ src/test/java/ResourceTest.java
@@ -1,6 +1,12 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import groovyjarjarantlr4.v4.runtime.misc.NotNull;
+
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+import org.hamcrest.core.IsEqual;
 
 import io.quarkus.test.junit.QuarkusTest;
 
@@ -47,6 +53,18 @@ public class ResourceTest {
             .when().get("/manga/db")
             .then()
                 .statusCode(404);
+
+    }
+
+    @Test
+    @DisplayName("Teste 3")
+    void testAPI(){
+
+        given()
+            .when().get("/manga/search/128991")
+            .then()
+           // .body("data.mal_id[0]", is("133666"));
+            .body(is(null));
 
     }
 
